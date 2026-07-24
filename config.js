3376a5ed219ec1f67a9de6cfc0fa0b3265a14a2f
noahmarquis2002-wq <noahmarquis2002@gmail.com>
2026-07-24 12:41:32 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+export default {
+  serveurId: "1526860062503600148",
+
+  salons: {
+    dinosTerrestres: "dinos-terrestres",
+    dinosVolants: "dinos-volants",
+    dinosAquatiques: "dinos-aquatiques",
+    equipements: "equipements",
+    ressources: "ressources",
+    transactions: "transactions",
+    avis: "avis-vendeurs",
+    demandes: "demande-dinos"
+  }
+};
